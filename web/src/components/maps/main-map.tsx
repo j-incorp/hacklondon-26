108b83bd8ce036ce7aaf50190e759d46256ed50a
Callum Shervill <shervc272@gmail.com>
2026-02-21 15:01:54 +0000

    chore: changed map style

--- web/src/components/maps/main-map.tsx
+++ web/src/components/maps/main-map.tsx
@@ -5,14 +5,9 @@ const MainMap = (): ReactElement => {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   )
 }
