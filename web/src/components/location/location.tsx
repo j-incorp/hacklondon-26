0153494a75d2a23af760b255427b0dc05fe93e78
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-02-21 13:42:38 +0000

    feat: loc provider

---
+++ web/src/components/location/location.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react'
+
+import { useLocation } from '@/hooks/use-location'
+
+const Location = (): ReactElement => {
+  const { location, loading, error } = useLocation()
+
+  if (loading) {
+    return <div>loading location...</div>
+  }
+
+  if (error) {
+    return <div>error loading location: {error.message}</div>
+  }
+
+  return (
+    <div>
+      lat: {location?.latitude} long: {location?.longitude}
+    </div>
+  )
+}
+
+export { Location }
