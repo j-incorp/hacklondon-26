25b62df33c52d58c41075b66196b90a39c79773e
JPadley18 <jacob5180@hotmail.co.uk>
2026-02-21 11:53:43 +0000

    gin init

--- api/internal/main.go
+++ api/internal/main.go
@@ -1,11 +1,27 @@
 package internal
 
 import (
+	"net/http"
+
 	"hacklondon26/internal/pkg/common"
 	"log/slog"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func Start() {
 	common.InitLogger()
 	slog.Info("Starting server")
+
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		slog.Error("server exited", "error", err)
+	}
 }
