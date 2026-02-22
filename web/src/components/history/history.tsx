57fac8bab34673ef70435db728d9af1045e65de6
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-02-22 02:09:16 +0000

    feaT: init history

---
+++ web/src/components/history/history.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from 'react'
+
+const History = (): ReactElement => {
+  return (
+    <div>
+      <h1>History</h1>
+      <p>This is the history page.</p>
+    </div>
+  )
+}
+
+export { History }
