bbe4be145d02542bcc0b7feca016d2f2231ea6c7
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-02-22 05:41:46 +0000

    feat: stuff

---
+++ web/src/components/cards/curses/cards.ts
@@ -0,0 +1,36 @@
+import type { Curse } from '../types'
+
+const curses: Curse[] = [
+  {
+    type: 'curse',
+    title: 'Orange curse',
+    curseType: 'orange',
+  },
+  {
+    type: 'curse',
+    title: 'Newspaper curse',
+    curseType: 'newspaper',
+  },
+  {
+    type: 'curse',
+    title: 'Fries curse',
+    curseType: 'fries',
+  },
+  {
+    type: 'curse',
+    title: 'Taxi curse',
+    curseType: 'taxi',
+  },
+  {
+    type: 'curse',
+    title: 'Rhyme curse',
+    curseType: 'rhyme',
+  },
+  {
+    type: 'curse',
+    title: 'Signal fault',
+    curseType: 'signal-fault',
+  },
+]
+
+export { curses }
